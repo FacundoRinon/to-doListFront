cf2a77df6cd089666603fcebf335b49ac6e2e095
FacundoRinon <facuzionff@gmail.com>
2024-06-27 13:10:22 -0300

    listCard updated to render data from lists

--- vite-project/src/components/ListCard/index.tsx
+++ vite-project/src/components/ListCard/index.tsx
@@ -1,17 +1,49 @@
+import React, { useEffect, useState } from "react";
 import "./index.scss";
 
-const ListCard = () => {
+interface List {
+  list_id: number;
+  title: string;
+  state: string;
+  type: string;
+  creation_date: string;
+  deadline: string;
+}
+
+interface ListCardProps {
+  list: List;
+}
+
+const ListCard: React.FC<ListCardProps> = ({ list }) => {
+  const [newDate, setNewDate] = useState("");
+  const [newDeadline, setNewDeadline] = useState("");
+
+  useEffect(() => {
+    const formatDate = (isoDate: string): string => {
+      const date = new Date(isoDate);
+      const day = date.getDate().toString().padStart(2, "0");
+      const month = (date.getMonth() + 1).toString().padStart(2, "0");
+      const year = date.getFullYear().toString();
+      return `${day}/${month}/${year}`;
+    };
+
+    const formattedDate = formatDate(list.creation_date);
+    const formattedDeadline = formatDate(list.deadline);
+    setNewDate(formattedDate);
+    setNewDeadline(formattedDeadline);
+  }, [list.creation_date]);
+
   return (
     <>
       <div className="listCard">
         <div className="listCard__title">
-          <h2>Titulo de lista</h2>
+          <h2>{list.title}</h2>
         </div>
         <div className="listCard__data">
-          <p>Type: ----------</p>
-          <p>Creation Date: -----</p>
-          <p>Deadline: ---------</p>
-          <p>State: -------</p>
+          <p>Type: {list.type}</p>
+          <p>Creation Date: {newDate}</p>
+          <p>Deadline: {newDeadline}</p>
+          <p>State: {list.state}</p>
         </div>
         <div className="listCard__delete">
           <h3>Delete</h3>
--- vite-project/src/pages/Lists/index.tsx
+++ vite-project/src/pages/Lists/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 import ListCard from "../../components/ListCard";
 import ListModal from "../../components/ListModal";
@@ -6,6 +7,27 @@ import ListModal from "../../components/ListModal";
 import "./index.scss";
 
 const Lists = () => {
+  interface List {
+    list_id: number;
+    title: string;
+    state: string;
+    type: string;
+    creation_date: string;
+    deadline: string;
+  }
+
+  interface RootState {
+    user: {
+      email: string;
+      id: number;
+      token: string;
+      lists: List[];
+      username: string;
+    };
+  }
+
+  const user = useSelector((state: RootState) => state.user);
+
   const [modal, setModal] = useState(false);
 
   return (
@@ -17,11 +39,26 @@ const Lists = () => {
           <p onClick={() => setModal(true)}>Create new list</p>
         </div>
         <div className="lists__lists">
-          <ListCard />
-          <ListCard />
-          <ListCard />
-          <ListCard />
-          <ListCard />
+          {user &&
+            user.lists.map((list) => (
+              <ListCard key={list.list_id} list={list} />
+            ))}
+          {user &&
+            user.lists.map((list) => (
+              <ListCard key={list.list_id} list={list} />
+            ))}
+          {user &&
+            user.lists.map((list) => (
+              <ListCard key={list.list_id} list={list} />
+            ))}
+          {user &&
+            user.lists.map((list) => (
+              <ListCard key={list.list_id} list={list} />
+            ))}
+          {user &&
+            user.lists.map((list) => (
+              <ListCard key={list.list_id} list={list} />
+            ))}
         </div>
       </div>
     </>
